372824cb52dd81ed45cd44d66d4524fe57ae0b90
n.marshanskiy <n.marshanskiy@altezza.board>
2023-04-26 20:24:16 +0300

    fix: restore method

--- main.py
+++ main.py
@@ -164,12 +164,12 @@ class TransferData:
         print('Restoring dump...')
         self.restore_dump()
         print('Restoring done.')
-        print('Removing old files...')
-        self.remove_old_files()
-        print('Removing done.')
-        # print('Clearing tables in db...')
-        # self.clear_tables()
-        # print('Clearing dome...')
+        # print('Removing old files...')
+        # self.remove_old_files()
+        # print('Removing done.')
+        print('Clearing tables in db...')
+        self.clear_tables()
+        print('Clearing dome...')
         print('Dont forget to execute command "python manage.py migrate --fake" in your project folder.')
 
 
